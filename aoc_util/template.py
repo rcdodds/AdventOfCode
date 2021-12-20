849d6eee0320d832d8ee49884c35bd6b74794eb2
rcdodds <rcdodds@gmail.com>
2021-12-19 23:59:59 -0500

    Updating template

--- aoc_util/template.py
+++ aoc_util/template.py
@@ -1,10 +1,11 @@
 # Libraries
-import numpy as np
 from aocd.models import Puzzle
 from aocd import submit
 
 
 def solve_puzzle(pzl_data, letter):
+    # Format puzzle input
+    puzzle_data = pzl_data.split('\n')
 
     if letter == 'A':
         return 0
@@ -15,19 +16,16 @@ def solve_puzzle(pzl_data, letter):
 
 if __name__ == '__main__':
     # Puzzle info
-    (year, day) = (2021, 12)
+    ready_to_solve = False
+    (year, day) = (2021, 20)
     puzzle = Puzzle(year=year, day=day)
     puzzle_solution = {'A': {'solved': puzzle.answered_a, 'guess': 0}, 'B': {'solved': puzzle.answered_b, 'guess': 0}}
-    ready_to_solve = False
-
-    # Format puzzle input
-    puzzle_data = puzzle.input_data.split('\n')
 
     # Consider both puzzles
     for part in ['A', 'B']:
         if not puzzle_solution[part]['solved']:
             # Attempt solution
-            guess = solve_puzzle(puzzle_data, part)
+            guess = solve_puzzle(puzzle.input_data, part)
             puzzle_solution[part]['guess'] = guess
             print('Guess for puzzle {}: {}'.format(part, guess))
 
